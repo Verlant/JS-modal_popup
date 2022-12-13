ad9b7fe5664d31050f5c060c4393554f45ffa713
Verlant <antoine.verlyck@gmail.com>
2022-12-13 10:10:49 +0100

    exo termine

--- main.js
+++ main.js
@@ -6,23 +6,26 @@ BTN_CLOSE.textContent = "Fermeture";
 document.body.appendChild(BTN_OPEN);
 document.body.appendChild(BTN_CLOSE);
 
-let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-width=600,height=300,left=100,top=100`;
+let params =
+  "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=600,height=300,left=100,top=100";
 let popup;
 let start;
 let end;
 
 BTN_OPEN.addEventListener("click", (e) => {
-  console.time("Popup");
+  popup = open("", "test", params);
   start = Date.now();
-  popup = open("/", "test", params);
+  document.body.removeChild(document.body.lastChild);
+  console.log(document.body.childNodes);
 });
 
 BTN_CLOSE.addEventListener("click", (e) => {
   popup.close();
-  console.timeEnd("Popup");
   end = Date.now();
   console.log(`Execution time: ${end / 1000 - start / 1000} s`);
+  document.body.append(
+    `Execution time: ${Math.round(end / 1000 - start / 1000)} s`
+  );
 });
 
 BTN_OPEN.style.margin = "10px";
